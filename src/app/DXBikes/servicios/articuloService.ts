ea866a8c2b3473352e2f72998c5d21617a594160
Franco-Caviglia <francocaviglia77@gmail.com>
2023-06-24 21:23:21 -0300

    asdf

--- src/app/DXBikes/servicios/articuloService.ts
+++ src/app/DXBikes/servicios/articuloService.ts
@@ -7,8 +7,12 @@ export class ArticuloService{
 
     constructor(){
         this.articulos = [
-            new ArticuloModel( 'Patriot', '2.1kg', 53994, 'Cuadro',  '' , '../assets/Cuadros/Patriot.jpg', '29'),
-            new ArticuloModel( 'Patriot', '2.1kg', 53994, 'Cuadro',  '' , '../assets/Cuadros/Patriot.jpg', '29')
+            new ArticuloModel( 'Patriot', 53994, 'Cuadro',  '' , '../assets/Cuadros/Patriot.jpg', '29', '','2.1kg'),
+            new ArticuloModel( 'Scott', 185000, 'Cuadro',  '' , '../assets/Cuadros/Scott.jpg', '29', '','1.9kg'),
+            new ArticuloModel( 'Venzo', 199200, 'Cuadro',  '' , '../assets/Cuadros/Venzo.jpg', '29', '', '1.7kg'),
+            new ArticuloModel( 'Foxter 4.20', 30987, 'Rueda', 'Aluminio', '../assets/Ruedas/R26.jpg', '', '26', ''),
+            new ArticuloModel( 'Vrc 701 Houston', 16500, 'Rueda', 'Aluminio', '../assets/Ruedas/R28.jpg', '', '28', ''),
+            new ArticuloModel( 'Shimano Deore Xt', 158336, 'Rueda', 'Aluminio', '../assets/Ruedas/R29.jpg', '', '29', '')
         ]
     }
 
