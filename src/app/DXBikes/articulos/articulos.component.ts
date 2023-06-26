088889b8f752c93a91fc400a568562ce44208e86
matiasabratteutn <matiasabratteutn@gmail.com>
2023-06-25 22:37:42 -0300

    barra de busqueda

--- src/app/DXBikes/articulos/articulos.component.ts
+++ src/app/DXBikes/articulos/articulos.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ArticuloService } from '../servicios/articuloService';
 import { ArticuloModel } from '../model/articulo-model';
 import { CarritoService } from '../servicios/carrito-service';
+import { ThisReceiver } from '@angular/compiler';
 
 @Component({
   selector: 'app-articulos',
@@ -12,16 +13,18 @@ export class ArticulosComponent implements OnInit{
   title = 'Articulos';
 
   public articulos!: Array<ArticuloModel>
-  
+
   //variables para la busqueda;
   public listaCoincidencias: Array<any> = [];
   public articuloBuscado: any; //Este va a ser llamado desde el input del html;
   public articuloEncontrado: any;
+  public ruedaFiltrada: boolean = false //nos va a decir si el checkbox del filtro de ruedas esta tildado
+  public cuadroFiltrado: boolean = false // nos va a decir si el checkbox de filtro de ruedas esta tildado
 
-  
+
   // @Input() dataEntrante: any;
   public listaArticulos: Array<any> = [];
- 
+
 
   constructor(
     // private servicioCarrito: ServicioCarritoService,
@@ -33,23 +36,37 @@ export class ArticulosComponent implements OnInit{
 
   ngOnInit(): void {
     this.articulos = this.servicioArticulo.getArticulos();
-    
+
   }
 
   agregarCarrito(articulo: any){
     this.servicioCarrito.addArticulo(articulo)
   }
-  
+
   buscarArticulo(){
     const x = this.articulos.find((elemento) => {
       if(elemento.marca === this.articuloBuscado){
         this.articuloEncontrado = this.articulos.indexOf(elemento);
         this.listaCoincidencias.push(this.articulos[this.articuloEncontrado]);
       }
+
+      // if(this.ruedaFiltrada == true)[
+      //   this.articuloEncontrado = this.articulos.indexOf(elemento)
+      // ]
+
     })
   }
-  
-  
+
+  // filtroRueda(){
+
+  //   if(this.ruedaFiltrada == false){
+  //     this.ruedaFiltrada = true
+  //   }else{
+  //     this.ruedaFiltrada = false
+  //   }
+
+
 }
+
 
 
